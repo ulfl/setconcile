94b91b319eaeeda25176487460afba5059a678c7
Ulf Leopold <336dev@gmail.com>
2015-09-30 22:33:02 +0200

    Update AMIs.

--- system_test/tf_replication_node/instance.tf
+++ system_test/tf_replication_node/instance.tf
@@ -5,12 +5,14 @@ variable "key_file" {}
 variable "region" { }
 variable "amis" {
     default = {
-#        us-east-1 = "ami-51ef9534" # f862c1e-1M-0.1-4096-nodeb
+       eu-west-1 = "ami-fda2888a" # eec22ce-1M-0.1-4096-nodea
+       us-east-1 = "ami-332d6a56" # eec22ce-1M-0.1-4096-nodeb
 #        eu-west-1 = "ami-9bc8e3ec" # f862c1e-1M-0.1-4096-nodea
+#        us-east-1 = "ami-51ef9534" # f862c1e-1M-0.1-4096-nodeb
 #        eu-west-1 = "ami-c796beb0" # 12d3e70-1M-0.001-4096-nodea.
 #        us-east-1 = "ami-4998e32c" # 12d3e70-1M-0.001-4096-nodeb.
-        us-east-1 = "ami-2dcf7b46" # ubuntu 14.04.3
-        eu-west-1 = "ami-92401ce5" # ubuntu 14.04.3
+#        eu-west-1 = "ami-92401ce5" # ubuntu 14.04.3
+#        us-east-1 = "ami-2dcf7b46" # ubuntu 14.04.3
     }
 }
 
@@ -35,10 +37,10 @@ resource "aws_instance" "replication_node" {
         destination = "riak.patch"
     }
 
-    provisioner "remote-exec" {
-        connection {user = "ubuntu" key_file = "${var.key_file}"}
-        script = "${path.module}/setup.sh"
-    }
+    # provisioner "remote-exec" {
+    #     connection {user = "ubuntu" key_file = "${var.key_file}"}
+    #     script = "${path.module}/setup.sh"
+    # }
 
     # Run separately so that the limits.conf changes are in affect.
     provisioner "remote-exec" {
