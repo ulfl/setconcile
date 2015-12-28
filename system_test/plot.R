1b02fac1dd8d15cf9d2f352345ccb4734e8881c3
Ulf Leopold <336dev@gmail.com>
2015-12-28 09:37:13 +0100

    Update graphs.

--- system_test/plot.R
+++ system_test/plot.R
@@ -14,12 +14,14 @@ prep <- ggplot(data, aes(factor(""), prep.time)) + geom_boxplot() + coord_flip()
 
 sync <- ggplot(data, aes(x=false.probability, y=time)) + geom_point(alpha = 0.5) + stat_smooth() + ylab("time (s)") + xlab("Bloom false probability") + ggtitle("Sync time")
 
-bsize <- ggplot(data, aes(x=false.probability, y=bloom.size/(1024*1024))) + geom_point(alpha = 0.5) + stat_smooth() + ylab("Size (MB)") + xlab("Bloom false probability") + ggtitle("Bloom Size")
+its <- ggplot(data, aes(x=false.probability, y=its)) + geom_point(alpha = 0.5) + stat_smooth() + ylab("Iterations") + xlab("Bloom false probability") + ggtitle("Number of iterations")
 
-dsize <- ggplot(data, aes(x=false.probability, y=data.size/(1024*1024))) + geom_point(alpha = 0.5) + stat_smooth() + ylab("Size (MB)") + xlab("Bloom false probability") + ggtitle("Data Size")
+bsize <- ggplot(data, aes(x=false.probability, y=bloom.size/(1024*1024))) + geom_point(alpha = 0.5) + stat_smooth() + ylab("Size (MB)") + xlab("Bloom false probability") + ggtitle("Bloom filters exchanged")
+
+dsize <- ggplot(data, aes(x=false.probability, y=data.size/(1024*1024))) + geom_point(alpha = 0.5) + stat_smooth() + ylab("Size (MB)") + xlab("Bloom false probability") + ggtitle("Data exchanged")
 
 outfile <- sprintf("%s.png", file)
 
 png(filename = outfile, width = 1200, height = 600)
-grid.arrange(prep, sync, bsize, dsize, ncol=2, nrow=2)
+grid.arrange(prep, sync, its, bsize, dsize, ncol=3, nrow=2)
 dev.off()
